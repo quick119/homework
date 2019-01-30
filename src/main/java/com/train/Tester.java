f590707c305dcf6b1e9a891410e73e540cd91f19
Simon Wang <quick119@hotmail.com>
2019-01-30 11:03:55 +0800

    done

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -1,7 +1,15 @@
 package com.train;
 
+import java.util.Scanner;
+
 public class Tester {
     public static void main(String[] args) {
-
+        System.out.println("Please enter number of tickets: ");
+        Scanner scanner = new Scanner(System.in);
+        int tickets = scanner.nextInt();
+        System.out.println("How many round-trip tickets: ");
+        int round_trip = scanner.nextInt();
+        TicketJ ticketJ = new TicketJ(tickets, round_trip);
+        ticketJ.print();
     }
 }
--- src/main/java/com/train/TicketJ.java
+++ src/main/java/com/train/TicketJ.java
@@ -3,16 +3,23 @@ package com.train;
 public class TicketJ {
     int tickets;
     int round_trip;
+    int total;
 
     public TicketJ(int tickets, int round_trip) {
         this.tickets = tickets;
         this.round_trip = round_trip;
     }
 
-    private int total() {
+    public int total() {
         int price = 1000;
         float discount = 0.9f;
-        int total = (tickets - round_trip)*price + (int)(round_trip*2*discount);
+        int total = (tickets - round_trip)*price + (int)(round_trip*2*price*discount);
         return total;
+    }
+
+    public void print() {
+        System.out.println("Total tickets: " + tickets + "\n"
+                + "Round-trip: " + round_trip + "\n"
+                + "Total: " + total());
     }
 }
